5600fdcdfc94c93541179d0949cc5d1d1a9c6da7
Vanja Cosic <vanjacosic@gmail.com>
2017-02-03 16:12:08 +0100

    Quick routing cleanup

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, IndexRedirect } from 'react-router';
+import { Router, Route } from 'react-router';
 
 import App from './components/App';
 import PackageSearch from './components/PackageSearch';
