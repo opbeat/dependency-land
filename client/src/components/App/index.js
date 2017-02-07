d4a059b9c5c51cbabd0b86684b2bc32e6ad548b2
Vanja Cosic <vanjacosic@gmail.com>
2017-02-07 13:21:14 +0100

    Standardize App, SearchInfo and NotFound 🔨

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
-import logo from './logo.svg';
-import './style.css';
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import logo from './logo.svg'
+import './style.css'
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <Link to="/">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <h1>Dependency.land <span>Beta</span></h1>
-                    </Link>
-                    <div className="App-repo"><i className="github icon"></i> Contribute on <a href="https://github.com/opbeat/dependency-land" target="_blank">GitHub</a></div>
-                    <div className="App-sponsor"><i className="heart icon"></i> Community project by <a href="https://opbeat.com" target="_blank">Opbeat</a></div>
-                </div>
-                <div className="ui container">
-                    {this.props.children}
-                </div>
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <Link to='/'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h1>Dependency.land <span>Beta</span></h1>
+          </Link>
+          <div className='App-repo'><i className='github icon' /> Contribute on <a href='https://github.com/opbeat/dependency-land' target='_blank'>GitHub</a></div>
+          <div className='App-sponsor'><i className='heart icon' /> Community project by <a href='https://opbeat.com' target='_blank'>Opbeat</a></div>
+        </div>
+        <div className='ui container'>
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- client/src/components/SearchInfo/index.js
+++ client/src/components/SearchInfo/index.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React from 'react'
+import { Link } from 'react-router'
 
 const SearchInfo = () => {
-
-    return (
-        <div className="ui text container">
-            <div className="ui stackable grid">
-                <div className="eight wide column">
-                    <b>Search for npm package dependents</b><br/>
-                    Enter the name of any npm package name <br/> and an optional semver range below.
-                </div>
-                <div className="eight wide column">
-                    <b>Try these examples:</b><br/>
-                    <Link to="/level/0.10.0">
-                        <code>level@0.10.0</code>
-                    </Link>
-                    &nbsp;or&nbsp;
-                    <Link to="/hypercore/^4.1.0">
-                        <code>hypercore@^4.1.0</code>
-                    </Link>
-                    <br/>
-                    <Link to="/standard">
-                        <code>standard@*</code>
-                    </Link>
-                    &nbsp;or&nbsp;
-                    <Link to="/array-flatten/~2.1">
-                        <code>array-flatten@~2.1</code>
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className='ui text container'>
+      <div className='ui stackable grid'>
+        <div className='eight wide column'>
+          <b>Search for npm package dependents</b><br />
+          Enter the name of any npm package name <br /> and an optional semver range below.
         </div>
-    );
+        <div className='eight wide column'>
+          <b>Try these examples:</b><br />
+          <Link to='/level/0.10.0'>
+            <code>level@0.10.0</code>
+          </Link>
+          &nbsp;or&nbsp;
+          <Link to='/hypercore/^4.1.0'>
+            <code>hypercore@^4.1.0</code>
+          </Link>
+          <br />
+          <Link to='/standard'>
+            <code>standard@*</code>
+          </Link>
+          &nbsp;or&nbsp;
+          <Link to='/array-flatten/~2.1'>
+            <code>array-flatten@~2.1</code>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-};
-
-export default SearchInfo;
+export default SearchInfo
