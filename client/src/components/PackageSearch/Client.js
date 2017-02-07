92bd60024bcb54d83cf7a9fdb932c6e2128ccac9
Vanja Cosic <vanjacosic@gmail.com>
2017-02-07 15:17:21 +0100

    Format and fix PackageSearch Client

--- client/src/components/PackageSearch/Client.js
+++ client/src/components/PackageSearch/Client.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-undef */
+import fetch from 'isomorphic-fetch'
+
 function search (query, range, cb) {
   query = encodeURIComponent(query)
   range = encodeURIComponent(range)
@@ -6,20 +7,20 @@ function search (query, range, cb) {
   return fetch(`/api/query/${query}${range ? '/' : ''}${range}`, {
     accept: 'application/json'
   }).then(checkStatus)
-      .then(parseJSON)
-      .then(cb)
+    .then(parseJSON)
+    .then(cb)
 }
 
 function checkStatus (response) {
-    // if (response.status >= 200 && response.status < 300) {
-    //     return response
-    // } else {
-    //     const error = new Error(`HTTP Error ${response.statusText}`)
-    //     error.status = response.statusText
-    //     error.response = response
-    //     console.log(error) // eslint-disable-line no-console
-    //     throw error
-    // }
+  // if (response.status >= 200 && response.status < 300) {
+  //     return response
+  // } else {
+  //     const error = new Error(`HTTP Error ${response.statusText}`)
+  //     error.status = response.statusText
+  //     error.response = response
+  //     console.log(error) // eslint-disable-line no-console
+  //     throw error
+  // }
   return response
 }
 
@@ -30,4 +31,5 @@ function parseJSON (response) {
 const Client = {
   search
 }
+
 export default Client
