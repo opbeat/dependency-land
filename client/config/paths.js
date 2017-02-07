4cba6d0d8828ee0e6acaddfaec0d44f3c8874a24
Vanja Cosic <vanjacosic@gmail.com>
2017-02-07 14:04:00 +0100

    Standardize client config folder 🔨

--- client/config/paths.js
+++ client/config/paths.js
@@ -1,11 +1,12 @@
-var path = require('path');
-var fs = require('fs');
+/* global resolveOwn */
+var path = require('path')
+var fs = require('fs')
 
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebookincubator/create-react-app/issues/637
-var appDirectory = fs.realpathSync(process.cwd());
-function resolveApp(relativePath) {
-  return path.resolve(appDirectory, relativePath);
+var appDirectory = fs.realpathSync(process.cwd())
+function resolveApp (relativePath) {
+  return path.resolve(appDirectory, relativePath)
 }
 
 // We support resolving modules according to `NODE_PATH`.
@@ -22,7 +23,7 @@ function resolveApp(relativePath) {
 var nodePaths = (process.env.NODE_PATH || '')
   .split(process.platform === 'win32' ? ';' : ':')
   .filter(Boolean)
-  .map(resolveApp);
+  .map(resolveApp)
 
 // config after eject: we're in ./config/
 module.exports = {
@@ -36,9 +37,7 @@ module.exports = {
   appNodeModules: resolveApp('node_modules'),
   ownNodeModules: resolveApp('node_modules'),
   nodePaths: nodePaths
-};
-
-
+}
 
 // config before publish: we're in ./packages/react-scripts/config/
 if (__dirname.indexOf(path.join('packages', 'react-scripts', 'config')) !== -1) {
@@ -53,5 +52,5 @@ if (__dirname.indexOf(path.join('packages', 'react-scripts', 'config')) !== -1) 
     appNodeModules: resolveOwn('../node_modules'),
     ownNodeModules: resolveOwn('../node_modules'),
     nodePaths: nodePaths
-  };
+  }
 }
