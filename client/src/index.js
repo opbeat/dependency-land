673aa35e2afd9bb8e50e70499a2634053077c902
Vanja Cosic <vanjacosic@gmail.com>
2017-02-07 13:56:38 +0100

    Standardize client index and routes 🔨

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,28 @@
-import initOpbeat from 'opbeat-react';
-import 'opbeat-react/router';
+import initOpbeat from 'opbeat-react'
+import 'opbeat-react/router'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { browserHistory } from 'react-router';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { browserHistory } from 'react-router'
 
-import Routes from './routes';
+import Routes from './routes'
 
-import './semantic-ui/semantic.min.css';
+import './semantic-ui/semantic.min.css'
 
-let opbeat_app_id = process.env.REACT_APP_OPBEAT_APP_ID;
-let opbeat_org_id = process.env.REACT_APP_OPBEAT_ORG_ID;
+let opbeatAppId = process.env.REACT_APP_OPBEAT_APP_ID
+let opbeatOrgId = process.env.REACT_APP_OPBEAT_ORG_ID
 
-if(process.env.NODE_ENV === 'production' && opbeat_app_id && opbeat_org_id) {
-    initOpbeat({
-        appId: opbeat_app_id,
-        orgId: opbeat_org_id,
-        performance: {
-            'initial-page-load': true
-        },
-    });
+if (process.env.NODE_ENV === 'production' && opbeatAppId && opbeatOrgId) {
+  initOpbeat({
+    appId: opbeatAppId,
+    orgId: opbeatOrgId,
+    performance: {
+      'initial-page-load': true
+    }
+  })
 }
 
 ReactDOM.render(
-    <Routes history={browserHistory} />,
-    document.getElementById('root')
-);
+  <Routes history={browserHistory} />,
+  document.getElementById('root')
+)
